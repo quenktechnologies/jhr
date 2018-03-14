9add4c05840de5170cdfd8b55e8a5c9b47c865e5
Lasana Murray <lmurray@quenk.com>
2018-03-14 18:14:15 -0400

    refactor: JSONTransform to new api.

---
+++ test/src/common/JSONTransform_test.ts
@@ -0,0 +1,41 @@
+import * as assert from 'assert';
+import * as must from 'must';
+import { JSONTransform } from '../../../src/agent';
+
+var trn;
+
+beforeEach(function() {
+
+    trn = new JSONTransform();
+})
+
+describe('JSONTransform', () => {
+
+    describe('#transformRequestBody()', () => {
+
+        it('should transform numbers', () =>
+            trn.transformRequestBody(1).then(value => assert(value === '1')))
+
+        it('should not transform strings', () =>
+            trn.transformRequestBody('{a:true}').then(value => assert(value === '{a:true}')))
+
+        it('should turn objects to strings', () =>
+            trn.transformRequestBody({ a: true }).then(value => assert(value === '{"a":true}')));
+
+        it('should preserve dates', () =>
+            trn.transformRequestBody({
+                a: new Date('2015-05-05T23:42:03.399Z')
+            })
+                .then(value => assert(value === '{"a":"2015-05-05T23:42:03.399Z"}')))
+
+    })
+
+    describe('#transformResponseBody()', () =>
+
+        it('should turn json strings to objects', () =>
+            trn
+                .transformResponseBody('{"a":true}')
+                .then(value => must(value).eql({ a: true }))))
+
+
+});
