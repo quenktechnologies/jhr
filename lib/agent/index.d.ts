43e8b4b59a84fae97c4727f4505c0dc589855c48
Lasana Murray <lmurray@quenk.com>
2020-09-20 22:04:01 -0400

    chore: rebuild

--- lib/agent/index.d.ts
+++ lib/agent/index.d.ts
@@ -27,34 +27,17 @@ export interface Options extends RequestOptions {
     port: number;
 }
 /**
- * Agent acts as an HTTP client.
- *
- * An Agent instance uses its transport to send HTTP requests
- * and receive responses.
+ * HTTPAgent is the API Agents provide.
  */
-export declare class Agent<ReqRaw, ResParsed> {
-    host: Host;
-    headers: OutgoingHeaders;
-    cookies: Container;
-    options: Partial<Options>;
-    transport: Transport<ReqRaw, ResParsed>;
-    plugins: Plugin<ReqRaw, ResParsed>[];
-    constructor(host: Host, headers: OutgoingHeaders, cookies: Container, options: Partial<Options>, transport: Transport<ReqRaw, ResParsed>, plugins: Plugin<ReqRaw, ResParsed>[]);
-    /**
-     * setTransport allows the transport to be changed (possibly to process
-     * a different type of response).
-     *
-     * A new Agent instance is created with NO plugins installed.
-     */
-    setTransport<Req, Res>(transport: Transport<Req, Res>, plugins?: Plugin<Req, Res>[]): Agent<Req, Res>;
+export interface HTTPAgent<ReqRaw, ResParsed> {
     /**
      * head request shorthand.
      */
-    head(path: Path, params?: Parameters, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    head(path: Path, params: Parameters, headers: OutgoingHeaders): Future<Response<ResParsed>>;
     /**
      * get request shorthand.
      */
-    get(path: Path, params?: Parameters, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    get(path: Path, params: Parameters, headers: OutgoingHeaders): Future<Response<ResParsed>>;
     /**
      * post request shorthand.
      */
@@ -71,8 +54,33 @@ export declare class Agent<ReqRaw, ResParsed> {
      * delete request shorthand.
      */
     delete(path: Path, body?: ReqRaw, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+}
+/**
+ * Agent acts as an HTTP client.
+ *
+ * An Agent instance uses its transport to send HTTP requests
+ * and receive responses.
+ */
+export declare class Agent<ReqRaw, ResParsed> implements HTTPAgent<ReqRaw, ResParsed> {
+    host: Host;
+    headers: OutgoingHeaders;
+    cookies: Container;
+    options: Partial<Options>;
+    transport: Transport<ReqRaw, ResParsed>;
+    plugins: Plugin<ReqRaw, ResParsed>[];
+    constructor(host: Host, headers: OutgoingHeaders, cookies: Container, options: Partial<Options>, transport: Transport<ReqRaw, ResParsed>, plugins: Plugin<ReqRaw, ResParsed>[]);
     /**
-     * send a Request to the server.
+     * setTransport allows the transport to be changed (possibly to process
+     * a different type of response).
+     *
+     * A new Agent instance is created with NO plugins installed.
      */
+    setTransport<Req, Res>(transport: Transport<Req, Res>, plugins?: Plugin<Req, Res>[]): Agent<Req, Res>;
+    head(path: Path, params?: Parameters, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    get(path: Path, params?: Parameters, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    post(path: Path, body?: ReqRaw, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    put(path: Path, body?: ReqRaw, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    patch(path: Path, body?: ReqRaw, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
+    delete(path: Path, body?: ReqRaw, headers?: OutgoingHeaders): Future<Response<ResParsed>>;
     send(req: Request<ReqRaw>): Future<Response<ResParsed>>;
 }
